177aa5b41cb0c06d9f54b56a2747bd06ee4d5290
kamitoshi <kroo.aq.74@gmail.com>
2020-06-02 09:52:32 +0900

    S3使用のための設定ファイル

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,18 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        # Amazon S3用の設定
-        :provider              => 'AWS',
-        :region                => ENV['S3_REGION'],
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-      config.storage :fog
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/pealco-image'
-    end
+    storage :fog
   else
     storage :file
   end
