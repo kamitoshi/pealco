c3f27d8e276c1970e4131a04fc5915714ad129c5
kamitoshi <kroo.aq.74@gmail.com>
2020-06-02 14:19:07 +0900

    S3設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,17 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production?
     config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    provider: 'AWS',
+    aws_access_key_id: ENV['S3_ACCESS_KEY'],
+    aws_secret_access_key: ENV['S3_SECRET_KEY'],
+    region: ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.asset_host = 'https://s3.amazonaws.com/#{ENV['S3_BUCKET']}'
+    config.fog_directory  = 'pealco-image'
+    config.asset_host = 'https://s3.amazonaws.com/pealco-image'
     config.fog_public = false
+  else
+    config.storage :file
   end
-else
-  storage :file
 end
